09da2c8f6d7517888f1301a31e6cedeabd21086e
Mafzii <23100040@lums.edu.pk>
2024-03-21 22:55:15 +0500

    Started JWT filter and logic

---
+++ src/main/java/com/mafzii/auth/config/JwtService.java
@@ -0,0 +1,39 @@
+package com.mafzii.auth.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "e9295f38b263f4d0298704881f7b226a733978f5ea041255d90f3937c4b9a0a7";
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    private Key getSignInKey() {
+        // possible issue with key
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
